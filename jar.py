8dc232b2ce8d6ab527c1d517ff08fd3e9fcc5f00
vedashree-design <chayams1986@gmail.com>
2026-08-27 09:59:18 +0000

    Set8 complete: jar done - 100%

---
+++ jar.py
@@ -0,0 +1,28 @@
+class Jar:
+    def __init__(self, capacity=12):
+        if capacity < 0:
+            raise ValueError("Invalid capacity")
+        self._capacity = capacity
+        self._size = 0
+
+    def __str__(self):
+        return "🍪" * self._size
+
+    def deposit(self, n):
+        if n < 0 or self._size + n > self._capacity:
+            raise ValueError("Too many cookies")
+        self._size += n
+
+    def withdraw(self, n):
+        if n < 0 or n > self._size:
+            raise ValueError("Not enough cookies")
+        self._size -= n
+
+    @property
+    def capacity(self):
+        return self._capacity
+
+    @property
+    def size(self):
+        return self._size
+        
